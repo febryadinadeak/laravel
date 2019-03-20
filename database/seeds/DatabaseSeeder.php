fffee12195ca87488a763c6f49e627f8504d0da7
febryadinadeak <febryadi.nadeak09@gmail.com>
2019-03-20 10:42:07 +0700

    modul 2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,14 @@
 <?php
-
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+/**
+* Seed the application's database.
+*
+* @return void
+*/
+public function run()
+{
+	$this->call(produkTableSeeder::class);
+}
 }
