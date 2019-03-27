1e599a8bb16bceb8e2f8508ac521e6235999c417
febryadinadeak <febryadi.nadeak09@gmail.com>
2019-03-27 11:05:00 +0700

    modul 3 routes

---
+++ htdocs/penjualan/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/index', function () {
+echo "<a href='".route('create')."'>Akses Route dengan nama </a>";
+});
+
+Route::get('/create', function () {
+echo "Route diakses menggunakan nama";
+})->name('create');
+
+Route::get('/produk', 'produkController@index');
+
+Route::any('/welcome' function () {
+//
+});
